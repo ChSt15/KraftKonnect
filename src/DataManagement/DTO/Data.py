923eeb47e549277d2a940f001105822ec702b89a
Christian Ruf <c.ruf99@gmail.com>
2021-07-20 16:32:34 +0200

    Added count for number of values for each source

--- src/DataManagement/DTO/Data.py
+++ src/DataManagement/DTO/Data.py
@@ -4,10 +4,11 @@ from time import time_ns
 
 @dataclass
 class Data:
-    __slots__ = ['id', 'source_id', 'set_id', 'timestamp', 'value']
+    __slots__ = ['id', 'source_id', 'set_id', 'timestamp', 'value_id', 'value']
     id: int
     source_id: int
     set_id: int
     timestamp: int
+    value_id: int
     value: str
 
--- src/DataManagement/DTO/Source.py
+++ src/DataManagement/DTO/Source.py
@@ -3,10 +3,11 @@ from dataclasses import dataclass
 
 @dataclass
 class Source:
-    __slots__ = ['id', 'name', 'description', 'script']
+    __slots__ = ['id', 'name', 'description', 'values', 'script']
     id: int
     name: str
     description: str
+    values: int
     script: str
 
 
