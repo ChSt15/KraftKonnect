49a782a21c392a370628ed8a265cb66c7dcb5f96
Christian Ruf <c.ruf99@gmail.com>
2021-07-20 16:43:08 +0200

    Renamed database data column value_id to key

--- src/DataManagement/DTO/Data.py
+++ src/DataManagement/DTO/Data.py
@@ -4,11 +4,11 @@ from time import time_ns
 
 @dataclass
 class Data:
-    __slots__ = ['id', 'source_id', 'set_id', 'timestamp', 'value_id', 'value']
+    __slots__ = ['id', 'source_id', 'set_id', 'key', 'value', 'timestamp']
     id: int
     source_id: int
     set_id: int
-    timestamp: int
-    value_id: int
+    key: int
     value: str
+    timestamp: int
 
