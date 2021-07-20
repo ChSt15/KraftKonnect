ca0fac04fb01eabf404f739f642482f45b3b0b00
Christian Ruf <c.ruf99@gmail.com>
2021-07-20 17:32:42 +0200

    Refactored MainWindow

--- src/core/MainWindow.py
+++ src/core/MainWindow.py
@@ -3,102 +3,97 @@ from PyQt5.QtCore import Qt
 from src.widgets.BasicPlot.BasicPlot import BasicPlot
 from src.widgets.WidgetContainer import Container
 from src.core.SourceManager.SourceManagerDialog import SourceManagerDialog
-from src.DataManagement.IO.OriginIO import OriginIO
 from src.DataManagement.IO.SetIO import SetIO
 from src.DataManagement.DataCollector.Collector import Collector
 from time import time_ns
 from src.DataManagement.DTO.Set import Set
 
+
 class CoreWindow(QMainWindow):
-
-
 
     def __init__(self):
         super().__init__()
 
         self.menu = self.menuBar()
-        self.setUpMenuBar()
-        self.configureLayout()
-        self.originIO = OriginIO()
-        self.setIO = SetIO()
-        self.currentSet = self.setIO.getLatest()
+        self.set_up_menu_bar()
+        self.configure_layout()
+        self.set_io = SetIO()
+        self.current_set = None
+        self.sources = []
         self.collectors = []
         self.containers = []
 
+    def set_up_menu_bar(self):
+        widget_menu = self.menu.addMenu('Widgets')
+        add_widget_menu = widget_menu.addMenu('Add')
+        basic_plot_widget_action = QAction('Basic Plot', self)
+        add_widget_menu.addAction(basic_plot_widget_action)
+        basic_plot_widget_action.triggered.connect(self.newBasicPlotWidget)
 
-    def setUpMenuBar(self):
-        widgetMenu = self.menu.addMenu('Widgets')
-        addWidgetMenu = widgetMenu.addMenu('Add')
-        basicPlotWidgetAction = QAction('Basic Plot', self)
-        addWidgetMenu.addAction(basicPlotWidgetAction)
-        basicPlotWidgetAction.triggered.connect(self.newBasicPlotWidget)
+        source_menu = self.menu.addMenu('Data')
+        action_manager = QAction('Manager', self)
+        action_manager.triggered.connect(self.launchSourceManager)
+        source_menu.addAction(action_manager)
 
-        sourceMenu = self.menu.addMenu('Data')
-        actionManager = QAction('Manager', self)
-        actionManager.triggered.connect(self.launchSourceManager)
-        sourceMenu.addAction(actionManager)
-
-        runMenu = self.menu.addMenu('Run')
-        runStartAction = QAction('Start', self)
-        runStopAction = QAction('Stop', self)
-        runStartAction.triggered.connect(self.setStart)
-        runStopAction.triggered.connect(self.setStop)
-        runMenu.addAction(runStartAction)
-        runMenu.addAction(runStopAction)
+        run_menu = self.menu.addMenu('Run')
+        run_start_action = QAction('Start', self)
+        run_stop_action = QAction('Stop', self)
+        run_start_action.triggered.connect(self.set_start)
+        run_stop_action.triggered.connect(self.setStop)
+        run_menu.addAction(run_start_action)
+        run_menu.addAction(run_stop_action)
 
     # Open manager to delete, rename, config origins and sources
-    def launchSourceManager(self):
+    @staticmethod
+    def launch_source_manager():
         sm = SourceManagerDialog()
         sm.exec_()
 
     # Layout specific configs
-    def configureLayout(self) -> None:
+    def configure_layout(self) -> None:
         self.setDockNestingEnabled(True)
         self.showMaximized()
 
-    # Add new single-x-axis plot from menu bar
-    def newBasicPlotWidget(self) -> None:
-        basicBlot = BasicPlot()
-        self.attachWidget(basicBlot)
+    # TODO autmoatically add all widgets in default widgets folder
+    # # Add new single-x-axis plot from menu bar
+    # def newBasicPlotWidget(self) -> None:
+    #     basicBlot = BasicPlot()
+    #     self.attachWidget(basicBlot)
 
     # Add widget to screen
-    def attachWidget(self, widget: QWidget) -> None:
-        container = Container(widget, self.currentSet.id)
+    def attach_widget(self, widget: QWidget) -> None:
+        container = Container(widget)
         self.containers.append(container)
         self.addDockWidget(Qt.BottomDockWidgetArea, container)
 
     # Start data recording, logging, displaying
-    def setStart(self) -> None:
-        setId = self.setIO.getNextSetId()
-        self.setGlobalContainerId(setId)
-        self.startContainerUpdates()
-        origins = self.originIO.getAll()
-        set = Set(id=setId)
-        set.id = self.currentSet.id + 1
-        set.start = time_ns()
-        self.currentSet = set
-        for origin in origins:
-            collector = Collector(origin)
+    def start_set_recording(self) -> None:
+        next_set_id = self.setIO.get_next_set_id()
+        self.start_container_updates()
+        self.current_set = Set(next_set_id, int(time_ns()/1000), None)
+        for container in self.containers:
+            for source, _ in container.sources_and_keys:
+                if source not in self.sources:
+                    self.sources.append(source)
+        for source in self.sources:
+            collector = Collector(source)
             self.collectors.append(collector)
             collector.start()
 
-    # Stop data recording, logging, displaying
-    def setStop(self) -> None:
+    def stop_set_recording(self) -> None:
+        self.stopContainerUpdates()
+        self.currentSet.end_time_ms = int(time_ns()/1000)
         while len(self.collectors) > 0:
             collector = self.collectors.pop()
             collector.stop()
-        self.currentSet.end = time_ns()
         self.setIO.write(self.currentSet)
-        self.stopContainerUpdates()
 
-    def setGlobalContainerId(self, id):
-        for container in self.containers:
-            container.currentSetId = id
-
-    def stopContainerUpdates(self):
+    def stop_container_updates(self):
+        """ Start every registered container """
         for container in self.containers:
             container.stop()
 
-    def startContainerUpdates(self):
+    def start_container_updates(self):
+        """ Stop every registered container """
         for container in self.containers:
             container.start()
