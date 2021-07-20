bbd15f732053aeedab0bc6f6e0e8759d13991bc8
Christian Ruf <c.ruf99@gmail.com>
2021-07-20 20:30:32 +0200

    Refactored widget package

--- src/core/MainWindow.py
+++ src/core/MainWindow.py
@@ -1,6 +1,5 @@
 from PyQt5.QtWidgets import QMainWindow, QAction, QWidget
 from PyQt5.QtCore import Qt
-from src.widgets.BasicPlot.BasicPlot import BasicPlot
 from src.widgets.WidgetContainer import Container
 from src.core.SourceManager.SourceManagerDialog import SourceManagerDialog
 from src.DataManagement.IO.SetIO import SetIO
